efeb2f52c2871f677254d2e0abf7b48067f63900
Ramiro G <ramigglez@gmail.com>
2022-03-16 15:45:50 -0600

    first commit

---
+++ Connection.php
@@ -0,0 +1,96 @@
+<?php
+
+class Connection {
+
+    private PDO $connection;
+    private array $setting = [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'data_base' => 'web',
+        'port' => '3306',
+        'password' => '',
+        'user_name' => 'root',
+        'charset' => 'utf8mb4'
+    ];
+
+    public function __construct () {}
+
+    public function connect (int $mssg = 0) {
+        try {
+            $CONTROLLER = $this->setting['driver'];
+            $SERVER = $this->setting['host'];
+            $DATABASE = $this->setting['data_base'];
+            $PORT = $this->setting['port'];
+            $USER = $this->setting['user_name'];
+            $PASSWORD = $this->setting['password'];
+            $CHARSET = $this->setting['charset'];
+            //cadena de conexion 
+            $stringConnection = new Strings(
+                "{$CONTROLLER}:host={$SERVER}:{$PORT};". 
+                "dbname={$DATABASE};charset={$CHARSET}"
+            );
+            //crear conexion
+            $this->connection = new PDO($stringConnection->getTxt(), $USER, $PASSWORD);
+            $stringConnection->setTxt('Connected.');
+            if ($mssg === 1) {
+                echo $stringConnection->getTxt();
+            }else{
+                return $this->connection;
+            }
+        } catch (Exception $e) {
+            $error = new Strings(
+                'ERROR : No Connection.
+                <pre>'
+                .$e->getTraceAsString().
+                '</pre>'
+            );
+            echo $error->getTxt();
+        }
+    }
+
+    //SETTERS
+    public function set_driver (string $x) {
+        $this->setting['driver'] = $x;
+    }
+    public function set_host (string $x) {
+        $this->setting['host'] = $x;
+    }
+    public function set_data_base (string $x) {
+        $this->setting['data_base'] = $x;
+    }
+    public function set_port (string $x) {
+        $this->setting['port'] = $x;
+    }
+    public function set_password (string $x) {
+        $this->setting['password'] = $x;
+    }
+    public function set_user_name (string $x) {
+        $this->setting['user_name'] = $x;
+    }
+    public function set_charset (string $x) {
+        $this->setting['charset'] = $x;
+    }
+    //GETTERS
+    public function get_driver () {
+        return $this->setting['driver'];
+    }
+    public function get_host () {
+        return $this->setting['host'];
+    }
+    public function get_data_base () {
+        return $this->setting['data_base'];
+    }
+    public function get_port () {
+        return $this->setting['port'];
+    }
+    public function get_password () {
+        return $this->setting['password'];
+    }
+    public function get_user_name () {
+        return $this->setting['user_name'];
+    }
+    public function get_charset () {
+        return $this->setting['charset'];
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# Connection
+
+## Properties
+
+<pre>
+    $setting = [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'data_base' => 'web',
+        'port' => '3306',
+        'password' => '',
+        'user_name' => 'root',
+        'charset' => 'utf8mb4'
+    ];
+</pre>
+
+## Methods
+
+<ol>
+    <li>__construct () {}</li>
+    <li>connect (int $mssg = 0)</li>
+    <li>set_driver (string $x)</li>
+    <li>set_host (string $x)</li>
+    <li>set_data_base (string $x)</li>
+    <li>set_port (string $x)</li>
+    <li>set_password (string $x)</li>
+    <li>set_user_name (string $x)</li>
+    <li>set_charset (string $x)</li>
+    <li>get_driver ()</li>
+    <li>get_host ()</li>
+    <li>get_data_base ()</li>
+    <li>get_port ()</li>
+    <li>get_password ()</li>
+    <li>get_user_name ()</li>
+    <li>get_charset ()</li>
+</ol>
+
+## how to 
+
+<pre>
+    $connect = new Connection;
+
+    $connect->connect(1);
+</pre>
